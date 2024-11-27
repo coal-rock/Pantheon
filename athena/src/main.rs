2bb8de69a291a7f5669c912f0984c9121c2537ae
coal-rock <colebrazinski15@gmail.com>
2024-11-27 02:13:47 -0500

    placeholder

--- athena/src/main.rs
+++ athena/src/main.rs
@@ -1,9 +1,52 @@
+use patternfly_yew::prelude::*;
 use yew::prelude::*;
 
 #[function_component(App)]
 fn app() -> Html {
     html! {
-        <h1>{ "Hello World" }</h1>
+    <>
+        <PageSectionGroup>
+            <PageSection
+                r#type={PageSectionType::Default}
+                variant={PageSectionVariant::Light}
+                limit_width=true
+                sticky={[PageSectionSticky::Top]}
+            >
+                <Content>
+                    <Title size={Size::XXXXLarge}>
+                        { "Athena" }
+                    </Title>
+                </Content>
+            </PageSection>
+        </PageSectionGroup>
+
+        <div style="padding: 32px;">
+        <Flex>
+            <FlexItem>
+                <Card>
+                    <CardTitle>{"Agents"}</CardTitle>
+
+                    <CardBody>
+                        <TextInputGroup>
+                            <TextInputGroupMain
+                                placeholder="agent name"
+                                icon={Icon::Search}
+                            />
+                            <TextInputGroupUtilities>
+                                <Button icon={Icon::Times} variant={ButtonVariant::Plain} />
+                            </TextInputGroupUtilities>
+                        </TextInputGroup>
+                        <Divider r#type={DividerType::Hr} />
+                    </CardBody>
+                </Card>
+            </FlexItem>
+        </Flex>
+        </div>
+
+        <Content>
+        </Content>
+    </>
+
     }
 }
 
