1492d54255575ba85140b0ea571dc4237d1e921a
coal-rock <colebrazinski15@gmail.com>
2025-03-11 21:33:59 -0400

    purge yew

--- athena/src/components/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod agent_table;
-pub mod console;
-pub mod full_page;
-pub mod network_log;
-pub mod sidebar;
--- athena/src/pages/mod.rs
+++
@@ -1,6 +0,0 @@
-pub mod about;
-pub mod agent;
-pub mod home;
-pub mod settings;
-pub mod downloads;
-pub mod alerts;
