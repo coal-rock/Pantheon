46b6ab6dbab3adfb19f7c69c9c2c7b67266cfece
Ruby Engelhart <engelhartrueben@gmail.com>
2025-05-02 15:52:10 -0400

    talaria(std-lib): added initial attempt at each required function within sys library.

--- talaria/src/stdlib/sys.rs
+++ talaria/src/stdlib/sys.rs
@@ -6,13 +6,15 @@ use rhai::Module;
 #[export_module]
 pub mod sys {
 
+    // TODO: clean up imports.
+    use elevated_command; // 13.2 KiB (for elevation status)
     use std::{
         env::consts,
-        io::Error,
-        process::{self, Command, Output, Stdio},
+        io::{Error, ErrorKind},
+        process::{self},
         result::Result,
     };
-    use whoami; // 24.1 KiB
+    use whoami; // 24.1 KiB (for username and hostname)
 
     #[derive(Clone, PartialEq, Debug)]
     pub enum OsEnum {
@@ -24,15 +26,19 @@ pub mod sys {
         Other(String),
     }
 
-    /// Returns `OsEnmu` that describes the agents OS.
+    /// Returns `OsEnum` that describes the agents OS.
     ///
     /// > [!INFO]
     /// > If the agent is running something other than: Linux, Windows, MacOs, OpenBSD, or FreeBSD,
     /// > the type of OS will be stored in `OsEnum::Other`
+    ///
+    /// > [!INFO]
+    /// > Most flavors of linux (of course, not all tested) show up as "linux"
+    // NOTE: consts::FAMILY has been more useful than consts::OS
+    // consider switing this out?
     pub fn os_name() -> OsEnum {
         // possible values of consts::OS :
         //      https://doc.rust-lang.org/std/env/consts/constant.OS.html
-        // arch linux == linux
         match consts::OS {
             "linux" => OsEnum::Linux,
             "windows" => OsEnum::Windows,
@@ -50,7 +56,6 @@ pub mod sys {
     }
 
     /// Returns hostname of agent
-    ///
     /// > [!CAUTION]
     /// > This is failable
     #[rhai_fn(return_raw)]
@@ -62,52 +67,129 @@ pub mod sys {
     }
 
     /// Checks if user has admin privilidges or admin like privilidges
-    ///
     /// > [!CAUTION]
-    /// > Only works on linux at the moment
-    //  TODO: I think I made god cry
+    /// > Untested on Windows machines.
     #[rhai_fn(return_raw)]
     pub fn is_admin() -> Result<bool, Box<EvalAltResult>> {
-        match os_name() {
-            OsEnum::Linux => match run_command_capture_ouput("id", vec!["-u", username().as_str()])
-            {
-                Ok(output) => Ok(String::from_utf8_lossy(&output.stdout) == "0"),
-                Err(error) => Err(error.to_string().into()),
-            },
-            // TODO: implement other OS.
-            // windows is going to suck.
-            _ => Err("Not implmented for this OS".into()),
+        let family = consts::FAMILY;
+        let res = match family {
+            // could use /proc/self/ for unix, but this will suffice
+            // WARNING: UNTESTED on windows
+            "unix" | "windows" => Ok(elevated_command::Command::is_elevated()),
+            _ => Err(Error::new(
+                ErrorKind::Unsupported,
+                format!("unsupported OS {}", consts::FAMILY),
+            )),
+        };
+
+        match res {
+            Ok(result) => Ok(result),
+            Err(error) => Err(error.to_string().into()),
         }
     }
 
+    /// Runs the `reboot` command on a given machine
+    /// > [!CAUTION]
+    /// > Untested on Windows machines.
+    // NOTE: For unix, this is not the most correct means of going about this.
+    // Would instead target the d-bus, but this should suffice.
     #[rhai_fn(return_raw)]
     pub fn reboot() -> Result<(), Box<EvalAltResult>> {
-        match os_name() {
-            // OsEnum::Linux => run_command("shutdown", vec![""])?,
-            _ => Ok(()),
+        let res = match consts::FAMILY {
+            "unix" => process::Command::new("shutdown")
+                .args(vec!["-r", "now"])
+                .status(),
+            // WARNING: UNTESTED
+            "windows" => process::Command::new("shutdown").args(vec!["/r"]).status(),
+            _ => Err(Error::new(
+                ErrorKind::Unsupported,
+                format!("unsupported OS {}", consts::FAMILY),
+            )),
+        };
+
+        // QUESTION:
+        // Do we return an enum of the status to determine success? Might be more useful
+        // So on _exit_status == 0, we would return CMD_STATUS.ok
+        // Or    _exit_status != 0, we would return CMD_STATUS.not_ok
+        match res {
+            Ok(_exit_status) => Ok(()),
+            Err(error) => Err(error.to_string().into()),
         }
     }
 
+    /// Runs the `shutdown` command on a given machine
+    /// > [!CAUTION]
+    /// > Untested on Windows machines.
+    // NOTE: For unix, this is not the most correct means of going about this.
+    // Would instead target the d-bus, but this should suffice.
     #[rhai_fn(return_raw)]
     pub fn shutdown() -> Result<(), Box<EvalAltResult>> {
-        Ok(())
-    }
+        let res = match consts::FAMILY {
+            "unix" => process::Command::new("shutdown").args(vec!["now"]).status(),
+            // WARNING: UNTESTED
+            "windows" => process::Command::new("shutdown").args(vec!["/s"]).status(),
+            _ => Err(Error::new(
+                ErrorKind::Unsupported,
+                format!("unsupported OS {}", consts::FAMILY),
+            )),
+        };
 
-    #[rhai_fn(return_raw)]
-    pub fn uptime() -> Result<u64, Box<EvalAltResult>> {
-        Ok(64u64)
-    }
-
-    /// Equivalent to getcwd on Unix an GetCurrentDirectoryW on Windows
-    ///
-    /// > [!CAUTION]
-    /// > Can throw exceptions if their is not sufficent permmisions to access
-    #[rhai_fn(return_raw)]
-    pub fn hermes_dir() -> Result<String, Box<EvalAltResult>> {
-        let directory = std::env::current_dir();
-        match directory {
-            Ok(path) => Ok(path.display().to_string()),
+        // QUESTION:
+        // Do we return an enum of the status to determine success? Might be more useful
+        // So on _exit_status == 0, we would return CMD_STATUS.ok
+        // Or    _exit_status != 0, we would return CMD_STATUS.not_ok
+        match res {
+            Ok(_exit_status) => Ok(()),
             Err(error) => Err(error.to_string().into()),
+        }
+    }
+
+    /// Checks the uptime a of the machine in seconds
+    /// > [!CAUTION}
+    /// > Only supports unix machines. Windows will come later
+    #[rhai_fn(return_raw)]
+    pub fn uptime() -> Result<f32, Box<EvalAltResult>> {
+        let time_vec;
+        let uptime;
+        let init_result;
+        let family = consts::FAMILY;
+
+        // idiomatic? unsure.
+        if family == "unix" {
+            // attempt to read from `/prov/uptime`
+            init_result = std::fs::read_to_string("/proc/uptime");
+
+            // if `init_result` is Ok and is not empty, split into vector
+            if init_result.as_ref().is_ok_and(|s| !s.is_empty()) {
+                time_vec = init_result
+                    .as_ref()
+                    .unwrap()
+                    .trim()
+                    .split(" ")
+                    .collect::<Vec<&str>>();
+            } else {
+                return Err(init_result.unwrap_err().to_string().into());
+            }
+
+            // `/proc/uptime` carries both idle time and uptime.
+            // Our vec, then, must have lenght of 2, else something is wrong
+            if time_vec.len() == 2 {
+                // uptime lives in index 0
+                uptime = time_vec[0].parse::<f32>();
+            } else {
+                return Err("uptime is malformed. Expected [<uptime>, <idletime>]"
+                    .to_string()
+                    .into());
+            }
+
+            match uptime {
+                Ok(time) => Ok(time),
+                Err(error) => Err(error.to_string().into()),
+            }
+        } else {
+            // Working on Windows next. Don't have it in me at the moment - ruby
+            // NOTE: Not in love that this error format is different than the rest - ruby
+            return Err(Box::new(format!("unsupported OS: {}", family).into()));
         }
     }
 
@@ -122,8 +204,7 @@ pub mod sys {
     }
 
     /// Return `true` if the agent is running Linux
-    ///
-    /// > ![CAUTION]
+    /// > ![INFO]
     /// > Most flavors of linux are returned as just "linux".
     pub fn is_linux() -> bool {
         consts::OS == "linux"
@@ -133,23 +214,6 @@ pub mod sys {
     pub fn is_macos() -> bool {
         consts::OS == "macos"
     }
-
-    fn run_command_capture_ouput(cmd: &str, args: Vec<&str>) -> Result<process::Output, Error> {
-        Command::new(cmd)
-            .args(args)
-            .stdout(Stdio::piped())
-            .stderr(Stdio::piped())
-            .output()
-    }
-
-    // #[rhai_fn(return_raw)]
-    // fn run_command(cmd: &str, args: Vec<&str>) -> Result<(), Box<EvalAltResult>> {
-    //     let mut cmd = Command::new(cmd).args(args).spawn()?;
-    //
-    //     cmd.wait()?;
-    //
-    //     Ok(())
-    // }
 }
 
 #[cfg(test)]
@@ -191,5 +255,15 @@ mod tests {
     fn get_hostname() {
         let _ = hostname();
         assert_eq!(1, 1);
+    }
+
+    #[cfg(target_family = "unix")]
+    #[test]
+    fn test_uptime() {
+        let uptime = uptime();
+        match uptime {
+            Ok(res) => assert_ne!(res, 0.0f32),
+            Err(error) => panic!("uptime() failed for other reasons: {}", error),
+        };
     }
 }
