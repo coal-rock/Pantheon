5fe307599088f37e893329dd3b4697ea73f4e972
coal-rock <colebrazinski15@gmail.com>
2025-05-10 15:42:21 -0400

    feat(talaria, scripting): scaffold new error types + error class support

--- talaria/src/stdlib/error.rs
+++ talaria/src/stdlib/error.rs
@@ -1,12 +1,45 @@
 use rhai::plugin::*;
-use strum_macros::IntoStaticStr;
+use strum::EnumProperty;
+use strum_macros::{EnumProperty, IntoStaticStr};
 
 #[export_module]
 pub mod error {
-    #[derive(Debug, Clone, IntoStaticStr)]
+    #[derive(Debug, Clone, IntoStaticStr, EnumProperty)]
     pub enum Error {
+        #[strum(props(class = "fs"))]
         FsError(String),
+
+        #[strum(props(class = "fs"))]
+        FsNotFound(String),
+
+        #[strum(props(class = "fs"))]
+        FsPermissionDenied(String),
+
+        #[strum(props(class = "fs"))]
+        FsFilenameTooLong(String),
+
+        #[strum(props(class = "fs"))]
+        FsIsADirectory(String),
+
+        #[strum(props(class = "fs"))]
+        FsNotADirectory(String),
+
+        #[strum(props(class = "fs"))]
+        FsMalformedPath(String),
+
+        #[strum(props(class = "fs"))]
+        FsInvalidUTF8(String),
+
+        #[strum(props(class = "fs"))]
+        FsStorageFull(String),
+
+        #[strum(props(class = "fs"))]
+        FsReadOnlyFilesystem(String),
+
+        #[strum(props(class = "sys"))]
         SysError(String),
+
+        #[strum(props(class = "sys"))]
         SysUnsupportedError(String),
     }
 
@@ -17,6 +50,15 @@ pub mod error {
                 Position::NONE,
             )))
         }
+    }
+
+    /// Returns the class that this error belongs to
+    #[rhai_fn(get = "class", pure)]
+    pub fn get_error_type(error: &mut Error) -> String {
+        String::from(match error.get_str("class") {
+            Some(class) => class,
+            None => "unknown",
+        })
     }
 
     /// Returns a deterministic string that can be matched upon to identify error type
